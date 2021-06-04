8d7618cd72cddaec46e97646d670bffa7292645c
Tim Stallmann <tim@rad.cat>
2021-06-04 14:09:45 -0400

    Tweak deploy script

--- deploy-lambda.sh
+++ deploy-lambda.sh
@@ -3,7 +3,7 @@
 OLD_PWD=$PWD
 
 rm function.zip
-cd .venv/lib/python3.8/site-packages/ || exit
+cd $(pipenv --venv)/lib/python3.8/site-packages/ || exit
 zip -q -r9 "$OLD_PWD"/function.zip . -x psycopg2/\* || exit
 cd "$OLD_PWD"/aws_psycopg2 || exit
 zip -q -r9 "$OLD_PWD"/function.zip . -i psycopg2/\* || exit
